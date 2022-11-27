1f74004de8f306c148b464718c0637e9749ff6f3
Ertuğrul Özdemir <45506003+phantom32-0@users.noreply.github.com>
2022-11-27 17:30:41 +0000

    Update zigdownload.py

--- zigdownload.py
+++ zigdownload.py
@@ -14,12 +14,22 @@ masterversion = data['master']['version']
 
 downloadurl = data['master']['x86_64-linux']['tarball']
 
-if input(f"Zig Master: {masterversion} Download? [y/n]>").lower() != 'y':
+if input(f"Zig Master: {masterversion} Download? [y/n]> ").lower() != 'y':
     exit(0)
 
 if os.system(f"wget {downloadurl} -O zigdownload.tar.xz") != 0:
     print(f"Error while trying to download {downloadurl}")
     exit(1)
+
+if (os.path.exists('./zig')):
+    if input(f"The directory 'zig' already exists. Overwrite? [y/n]> ").lower() != 'y':
+        print("Error, 'zig' already exists.")
+        exit(1)
+    if os.system(f"rm -r ./zig/") != 0:
+        print("Error while deleting 'zig' directory.")
+        exit(1)
+
+os.system(f"mkdir zig");
 
 if os.system(f"tar xvf zigdownload.tar.xz -C ./zig") != 0:
     print(f"Error while trying to extract Zig")
